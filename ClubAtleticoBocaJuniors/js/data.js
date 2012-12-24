0efd9350981061d04435edf836e7684f8ba8a709
guille <guille@localhost>
2012-12-24 16:11:36 -0300

    Minor: rename

--- ClubAtleticoBocaJuniors/js/data.js
+++ ClubAtleticoBocaJuniors/js/data.js
@@ -121,11 +121,11 @@
                 });
         } else {
             readFile();
-            ShowConnectionError();
+            showConnectionError();
         }
     };
 
-    function ShowConnectionError() {
+    function showConnectionError() {
         var popup = Windows.UI.Popups.MessageDialog("Ha habido un error intentando acceder a los nuevos datos o no hay conexiones de red disponibles.\nPor favor asegúrese de contar con acceso de red y vuelva a abrir la aplicación.", "Sin conexión");
         popup.showAsync();
     }
