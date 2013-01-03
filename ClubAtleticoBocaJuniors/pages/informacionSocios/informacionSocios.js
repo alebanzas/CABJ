0122c601bdfe5166d9cde5b55b3a4a028247e6cf
Guillermo Bellmann <gbellmann@users.noreply.github.com>
2013-01-02 21:57:13 -0300

    Minor: indent

---
+++ ClubAtleticoBocaJuniors/pages/informacionSocios/informacionSocios.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+    
+    WinJS.UI.Pages.define("/pages/informacionSocios/informacionSocios.html", {
+        ready: function (element, options) {
+            
+        },
+    });
+
+})();
